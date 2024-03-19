e298944c734b74f983906509a16f262f3410b0a0
Saksham <sakshamshinde412@gmail.com>
2024-03-19 17:13:29 +0530

    Routes in React updated

---
+++ routing/src/components/About.jsx
@@ -0,0 +1,5 @@
+export function About() {
+    return <div>
+        <div>About page</div>
+    </div>
+}
---
+++ routing/src/components/Home.jsx
@@ -0,0 +1,5 @@
+export function Home() {
+    return <div>
+        <div>Home page</div>
+    </div>
+}
