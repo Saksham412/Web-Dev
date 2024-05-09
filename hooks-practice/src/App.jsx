39334608ff31ce7f6fb6d22024aae8c0a956d5ce
Saksham <sakshamshinde412@gmail.com>
2024-05-09 23:24:45 +0530

    custom hooks

---
+++ hooks-practice/src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function useTodos(n) {
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos)
+        setLoading(false)
+      })
+    },n*1000) 
+
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos)
+        setLoading(false)
+      })
+       
+      return () => {
+        clearInterval(value)
+      }
+  },[n])
+
+  return {todos, loading};
+}
+
+function App() {
+  const {todos, loading} = useTodos(2)
+  
+  if(loading){
+    return <div>Loading...</div>
+  }
+
+  return (
+    <>
+      {todos.map(todo => <Track todo={todo}/>)}
+    </>
+  )
+
+}
+
+function Track({todo}) {
+
+  return (
+    <div>
+      {todo.title}
+      <br />
+      {todo.description}
+    </div>
+  )
+
+}
+
+
+export default App
